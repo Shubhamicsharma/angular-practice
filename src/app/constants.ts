0df82a9d90c57829d5997274fcaa9e4418efc301
shubhamicsharma <shubhamicsharma@gmail.com>
2024-08-22 21:31:52 +0530

    Demo

---
+++ src/app/constants.ts
@@ -0,0 +1,14 @@
+interface UserData {
+    id: number;
+    first_name : String;
+    last_name : String;
+    email  :String;
+    contact: String;
+    address : String;
+    company : String;
+    designation : String;
+}
+
+interface CountryData {
+    country : String
+}
