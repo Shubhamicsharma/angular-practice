f03df05908afe69d1497e8151baf955e9ef37b40
shubhamicsharma <shubhamicsharma@gmail.com>
2024-12-21 21:06:21 +0530

    start push notification

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDPszet_Q-mxqu4sUVW6YOOAGypAW-e7UI",
+        authDomain: "fir-7c1c0.firebaseapp.com",
+        projectId: "fir-7c1c0",
+        storageBucket: "fir-7c1c0.firebasestorage.app",
+        messagingSenderId: "1022309262800",
+        appId: "1:1022309262800:web:4820e73f89265f2d13c5d5",
+        measurementId: "G-PBZGJ3QNDQ"
+    }
+};
